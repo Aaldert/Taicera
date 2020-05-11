ad1ed1af8ff52eacbe4d0af8694f11e522581a68
Daniel <daan@compagner.com>
2020-05-11 21:45:03 +0200

    Hello Tai commit

---
+++ Introduction/Introduction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Hello Tai");
+            System.Console.WriteLine("Hello Tai");
+            Console.ReadLine();
+        }
+    }
+}
